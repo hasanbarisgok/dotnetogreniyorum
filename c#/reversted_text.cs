d47c47a51f00f1d712fed6a3c5ce948bd16b1406
Hasan Barış GÖK <70600067+hasanbarisgok@users.noreply.github.com>
2023-06-24 15:06:31 +0300

    Create reversted_text.cs

---
+++ c#/reversted_text.cs
@@ -0,0 +1,24 @@
+using System;
+
+void Enter_Sentence()
+{
+    Console.Write("Enter the sentence you want to reverse");
+
+    string? text = Console.ReadLine();
+
+    Print_Reverse(text);
+}
+void Print_Reverse(string text)
+{
+    string reversted_text = "";
+
+    for (int i = text.Length - 1; i>=0; i--)
+    {
+        reversted_text += text[i];
+    }
+
+    Console.WriteLine($"Reversted Text: {reversted_text}");
+    
+}
+
+Enter_Sentence();
